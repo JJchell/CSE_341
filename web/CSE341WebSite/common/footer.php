12d6f13527248492e9413934968d094432077d1e
James C <jchell@byui.edu>
2020-05-01 18:32:52 -0500

    W2.2 commit

--- web/CSE341WebSite/common/footer.php
+++ web/CSE341WebSite/common/footer.php
@@ -1,3 +1,3 @@
 <p>&copy; James Chell, All rights reserved.</p>
-            <?php echo "The time is " . date("h:i:sa");?>
-            <p>Last updated 27 April, 2020</p>
+            <?php echo "The universal time is " . date("h:i:sa");?>
+            <p>Last updated 1 March, 2020</p>
