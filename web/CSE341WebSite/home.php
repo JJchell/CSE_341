3bdfff123b43de3be2a82843275ac3b76b2f798c
James C <jchell@byui.edu>
2020-05-01 20:10:18 -0500

    small changes

--- web/CSE341WebSite/home.php
+++ web/CSE341WebSite/home.php
@@ -28,9 +28,7 @@
       </main>
       <footer>
          <?php include 'common/footer.php'; ?>
-            <!-- <p>&copy; James Chell, All rights reserved.</p>
             <?php echo "The time is " . date("h:i:sa");?>
-            <p>Last updated 27 April, 2020</p> -->
          </footer>
    </body>
 </html>
