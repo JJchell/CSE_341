fac1390628610df69d257a815181ee8c5280069d
James C <jchell@byui.edu>
2020-05-23 15:01:31 -0500

    results page edit

--- web/CSE341WebSite/results.php
+++ web/CSE341WebSite/results.php
@@ -1,75 +1,72 @@
 <?php
-    
-    require "common/dbconnect.php";
-    $db=get_db();
-    
-​
-   
-    // function validateInput($data) {
-    //     $data = trim($data);
-    //     $data = stripslashes($data);
-    //     $data = htmlspecialchars($data);
-    //     return $data;
-    // }
-​
-    function searchQuery($name, $db) {
-   // $db=dbConnection();    
-    $stmt = $db->prepare('SELECT * FROM scout WHERE first_nsme = :name');
+require "common/dbconnect.php";
+$db = get_db();
+function validateInput($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+function searchQuery($name, $db)
+{
+    // $db=dbConnection();
+    $stmt = $db->prepare('SELECT * FROM scout WHERE first_name = :name');
     //$name= '$name';
     $stmt->bindValue(':name', $name, PDO::PARAM_STR);
     $stmt->execute();
-    $book = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $book;
-    }
-​
-    // If the page loads as a POST request, look for this variable, and if it is set
-if(isset($_POST['bookToFind'])) {
-    // This is just for testing to make sure we have the correct text
-    //echo "<h1>" . $_POST['bookToFind'] . "</h1>";
-    // Validate & sanitize the input
-    $searchText = validateInput($_POST['bookToFind']);
-    // Now run the query to find the text in the database, and then save the results as a variable
-    $books = searchQuery($searchText, $db);
-  // Change the method name
-  print_r($books);
-  
-​
-  }
-​
-?>
-
-<!DOCTYPE html>
-
-   <html lang="en-us">
-   <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Scout page | James Chell | CSE 341 BYUI</title>
-      <link rel="stylesheet" href="css/normalize.css">
-      <link rel="stylesheet" href="css/main.css">
-   </head>
-   <body>
-<header>
-   <?php include 'common/header.php'; ?>
-</header> 
-    <main>
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+}
+// If the page loads as a POST request, look for this variable, and if it is set
+if (isset($_POST['first_name'])) {
     
-        <fieldset>
-        
-        <p>Your results: </p>
-                <p>Scout name: <?=$name ?></p>
-                <p>E-mail: <?=$email ?></p>
-                <p>Cookie: <?=$cookie ?></p>
-        
-        </fieldset>
-            
+    $name = validateInput($_POST['first_name']);
+    $rows = searchQuery($name, $db);
+    print_r($rows);
+}
 
+if (isset($_POST['last_name'])) {
+    
+    $last_name = validateInput($_POST['last_name']);
+    // $rows = searchQuery($searchName, $db);
+    // print_r($rows);
+}
+
+if (isset($_POST['cookie'])) {
+    
+    $cookie = validateInput($_POST['cookie']);
+    // $rows = searchQuery($cookie, $db);
+    // print_r($rows);
+}
+?>
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Scout page | James Chell | CSE 341 BYUI</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header>
+        <?php include 'common/header.php'; ?>
+    </header>
+    <main>
+        <fieldset>
+        <?php foreach ($rows as $row){
+            echo $row['first_name'];
+        }?>
+            <p>Your results: </p>
+            <p>Scout name: <?= $name ?></p>
+            <p>Last name: <?= $last_name ?></p>
+            <p>Cookie: <?= $cookie ?></p>
+        </fieldset>
         </div>
     </main>
-        <footer>
-
-            <?php include 'common/footer.php'; ?>
-
-        </footer>
-   </body>
+    <footer>
+        <?php include 'common/footer.php'; ?>
+    </footer>
+</body>
 </html>
--- web/CSE341WebSite/scout.php
+++ web/CSE341WebSite/scout.php
@@ -21,8 +21,8 @@ $db=get_db();
         <fieldset>
         <div class="info">
             <form action="results.php" method="post">
-                Name: <input type="text" name="name"><br>
-                E-mail: <input type="text" name="email"><br>
+                Name: <input type="text" name="first_name"><br>
+                Last name: <input type="text" name="last_name"><br>
                 Cookie: <input type="text" name="cookie"><br>
                 <input type="submit" value="results" class="getBtn">
                 <input type="hidden" name="action" value=""><br>
