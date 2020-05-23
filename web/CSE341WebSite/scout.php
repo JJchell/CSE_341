93d1f39f3e60bba59058093320040495141192e2
James C <jchell@byui.edu>
2020-05-23 11:45:47 -0500

    scout page

---
+++ web/CSE341WebSite/scout.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+   <html lang="en-us">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Home page | James Chell | CSE 341 BYUI</title>
+      <link rel="stylesheet" href="css/normalize.css">
+      <link rel="stylesheet" href="css/main.css">
+   </head>
+   <body>
+<header>
+   <?php include 'common/header.php'; ?>
+</header> 
+    <main>
+        <div class="info">
+            <form action="welcome.php" method="post">
+                Name: <input type="text" name="name"><br>
+                E-mail: <input type="text" name="email"><br>
+                <input type="submit">
+            </form>
+        </div>
+    </main>
+        <footer>
+
+            <?php include 'common/footer.php'; ?>
+
+        </footer>
+   </body>
+</html>
