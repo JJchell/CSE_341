bacf16fd8134e870758c4fb3658cfbdbe09e38a9
James C <jchell@byui.edu>
2020-05-23 14:23:12 -0500

    results page edit

--- web/CSE341WebSite/results.php
+++ web/CSE341WebSite/results.php
@@ -1,3 +1,43 @@
+<?php
+    
+    require "common/dbconnect.php";
+    $db=get_db();
+    
+​
+   
+    // function validateInput($data) {
+    //     $data = trim($data);
+    //     $data = stripslashes($data);
+    //     $data = htmlspecialchars($data);
+    //     return $data;
+    // }
+​
+    function searchQuery($name, $db) {
+   // $db=dbConnection();    
+    $stmt = $db->prepare('SELECT * FROM scout WHERE first_nsme = :name');
+    //$name= '$name';
+    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    $stmt->execute();
+    $book = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $book;
+    }
+​
+    // If the page loads as a POST request, look for this variable, and if it is set
+if(isset($_POST['bookToFind'])) {
+    // This is just for testing to make sure we have the correct text
+    //echo "<h1>" . $_POST['bookToFind'] . "</h1>";
+    // Validate & sanitize the input
+    $searchText = validateInput($_POST['bookToFind']);
+    // Now run the query to find the text in the database, and then save the results as a variable
+    $books = searchQuery($searchText, $db);
+  // Change the method name
+  print_r($books);
+  
+​
+  }
+​
+?>
+
 <!DOCTYPE html>
 
    <html lang="en-us">
