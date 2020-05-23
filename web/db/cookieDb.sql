996d87bdf587c515f73290f274345d4e7f0769d3
James C <jchell@byui.edu>
2020-05-23 11:24:12 -0500

    Db changes

--- web/db/cookieDb.sql
+++ web/db/cookieDb.sql
@@ -45,3 +45,13 @@ INSERT INTO cookies(cookie_name, cookies_on_hand) VALUES ('Thanks-a-lot', 50);
 INSERT INTO cookies(cookie_name, cookies_on_hand) VALUES ('Lemonades', 50);
 INSERT INTO cookies(cookie_name, cookies_on_hand) VALUES ('Smores', 50);
 
+INSERT INTO scout(first_name, last_name, date_of_birth, user_name, password, email, phone) VALUES ('Jessie', 'Chell', '10-04-04', 'username', 'password', '123@email.com', 5550001);
+INSERT INTO scout(first_name, last_name, date_of_birth, user_name, password, email, phone) VALUES ('Samantha', 'Chell', '11-07-07', 'username', 'password', '123@email.com', 5550002);
+INSERT INTO scout(first_name, last_name, date_of_birth, user_name, password, email, phone) VALUES ('Jaen', 'Gray', '12-25-04', 'username', 'password', '123@email.com', 5550003);
+INSERT INTO scout(first_name, last_name, date_of_birth, user_name, password, email, phone) VALUES ('Carol', 'Danvers', '10-15-09', 'username', 'password', '123@email.com', 5550004);
+INSERT INTO scout(first_name, last_name, date_of_birth, user_name, password, email, phone) VALUES ('Wanda', 'Maximoff', '02-4-11', 'username', 'password', '123@email.com', 5550005);
+INSERT INTO scout(first_name, last_name, date_of_birth, user_name, password, email, phone) VALUES ('Emma', 'Frost', '08-05-10', 'username', 'password', '123@email.com', 5550006);
+INSERT INTO scout(first_name, last_name, date_of_birth, user_name, password, email, phone) VALUES ('Peggy', 'Carter', '04-10-08', 'username', 'password', '123@email.com', 5550007);
+INSERT INTO scout(first_name, last_name, date_of_birth, user_name, password, email, phone) VALUES ('MJ', 'Watson', '07-1-10', 'username', 'password', '123@email.com', 5550008);
+INSERT INTO scout(first_name, last_name, date_of_birth, user_name, password, email, phone) VALUES ('Natasha', 'Romanova', '05-26-06', 'username', 'password', '123@email.com', 5550009);
+INSERT INTO scout(first_name, last_name, date_of_birth, user_name, password, email, phone) VALUES ('Ann Marie', 'LeBeau', '06-14-04', 'username', 'password', '123@email.com', 5551010);
