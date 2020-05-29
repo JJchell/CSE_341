53dcb1e90f36e1995dc9946f0a4882c330a7baeb
James C <jchell@byui.edu>
2020-05-28 22:11:43 -0500

    add scout

--- web/CSE341WebSite/scout.php
+++ web/CSE341WebSite/scout.php
@@ -19,6 +19,50 @@ $db=get_db();
    <?php include 'common/header.php'; ?>
 </header> 
     <main>
+
+    <fieldset>
+                    <legend><span>Add Scout Information</span></legend>
+
+                    <div class="row">
+
+                        <div class="column">
+
+                            <label><span>First Name: </span><br>
+                                <input name="add_first_name" type="text" value="" id="add_first_name" placeholder="">
+                            </label><br><br>
+
+                            <label><span>Last Name: </span><br>
+                                <input name="add_last_name" type="text" value="" id="add_last_name" placeholder="">
+                            </label><br><br>
+
+                            <label><span>Date of Birth: </span><br>
+                                <input name="add_date_of-birth" type="text" value="" id="add_date_of-birth" placeholder="" required>
+                            </label><br><br>
+
+                            <label><span>Email: </span><br>
+                                <input name="add_email" type="text" value="" id="add_email" placeholder="" required>
+                            </label><br><br>
+
+                        </div>
+
+                            <label><span>Phone Number: </span><br>
+                                <input name="add_phone" type="number" value="" id="add_phone" placeholder=""required>
+                            </label><br><br>
+
+                            <label><span>User Name: </span><br>
+                                <input name="add_user_name" type="text" value="" id="add_user_name" placeholder="">
+                            </label><br><br>
+
+                            <label><span>Password: </span><br>
+                                <input name="add_password" type="number" value="" id="add_password" placeholder="">
+                            </label><br><br>
+
+                                </label>
+
+                                </div>
+
+
+                </fieldset>
         <fieldset>
         <div class="info">
             <form action="results.php" method="post">
