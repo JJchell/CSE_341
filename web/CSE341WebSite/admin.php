0486e23cae629cab97272c252bdc875faca1c33d
James C <jchell@byui.edu>
2020-05-29 18:50:45 -0500

    admin page added

--- web/CSE341WebSite/admin.php
+++ web/CSE341WebSite/admin.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
-    if (isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
-        header ("Location: scout.php");
-    }
+    // if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
+    //     header ("Location: scout.php");
+    // }
 
 
 ?>
