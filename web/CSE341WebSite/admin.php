b3365f66bf800e33924598b17a70383d5c5b8b3a
James C <jchell@byui.edu>
2020-05-30 18:14:31 -0500

    admin page added

--- web/CSE341WebSite/admin.php
+++ web/CSE341WebSite/admin.php
@@ -49,12 +49,7 @@ if (isset($_POST['submitted'])) {
 
 $db = get_db();
 
-// finally, redirect them to a new page to actually show the topics
-header("Location: show-topics.php");
-die(); // we always include a die after redirects. In this case, there would be no
-// harm if the user got the rest of the page, because there is nothing else
-// but in general, there could be things after here that we don't want them
-// to see.
+
 ?>
 <!DOCTYPE HTML>
 <html lang="en-us">
