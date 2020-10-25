51cd0d6cdce3c0ea0efb37a9e6870cc917f2edae
James C <jchell@byui.edu>
2020-10-25 18:31:52 -0500

    filler text

--- web/CSE341WebSite/home.php
+++ web/CSE341WebSite/home.php
@@ -32,9 +32,9 @@
                   </div>
 
                   <div class="mySlides fade">
-                  <div class="numbertext">3 / 3</div>
-                  <img src="images/cookie_pickup.jpeg" style="width:100%">
-                  <div class="text">Cookies!!</div>
+                     <div class="numbertext">3 / 3</div>
+                        <img src="images/cookie_pickup.jpeg" style="width:100%">
+                     <div class="text">Cookies!!</div>
                   </div>
 
                   <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
@@ -44,9 +44,9 @@
                   <br>
 
                   <div style="text-align:center">
-                  <span class="dot" onclick="currentSlide(1)"></span> 
-                  <span class="dot" onclick="currentSlide(2)"></span> 
-                  <span class="dot" onclick="currentSlide(3)"></span> 
+                     <span class="dot" onclick="currentSlide(1)"></span> 
+                     <span class="dot" onclick="currentSlide(2)"></span> 
+                     <span class="dot" onclick="currentSlide(3)"></span> 
                   </div>
 
                   
