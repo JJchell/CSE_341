9c0b7855b33c505e23764ae8142091a1af4cf3b6
James C <jchell@byui.edu>
2020-10-24 21:53:10 -0500

    new logo

--- web/CSE341WebSite/home.php
+++ web/CSE341WebSite/home.php
@@ -16,14 +16,41 @@
       <main>
          <fieldset>
             <div class="homePage">
-            <div class="info">
-               <img id="family" src="images/theChells.jpg" alt="me and the fam" >
-            </div>
-            <div class="info">
-            
-               Welcome to Girl Scout troup XXXX 
-                  
-            </div>
+               <div class="info">
+                  <!-- The grid: four columns -->
+               <div class="row">
+                  <div class="column">
+                     <img src="archer.jpg" alt="Nature" onclick="myFunction(this);">
+                  </div>
+                  <div class="column">
+                     <img src="GSleaders.jpeg" alt="Snow" onclick="myFunction(this);">
+                  </div>
+                  <div class="column">
+                     <img src="campfire.jpeg" alt="Mountains" onclick="myFunction(this);">
+                  </div>
+                  <div class="column">
+                     <img src="cookie_pickup.jpeg" alt="Lights" onclick="myFunction(this);">
+                  </div>
+               </div>
+
+               <!-- The expanding image container -->
+               <div class="container">
+               <!-- Close the image -->
+               <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+
+               <!-- Expanded image -->
+               <img id="expandedImg" style="width:100%">
+
+               <!-- Image text -->
+               <div id="imgtext"></div>
+               </div>
+               </div>
+               <!-- End the grid: four columns -->
+               <div class="info">
+               
+                  Welcome to Girl Scout troup XXXX 
+                     
+               </div>
             </div>
          </fieldset>
       </main>
