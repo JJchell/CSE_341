34342373fa109611542e629c141f757f84e35b59
James C <jchell@byui.edu>
2020-10-24 21:20:33 -0500

    new logo

--- web/CSE341WebSite/home.php
+++ web/CSE341WebSite/home.php
@@ -21,12 +21,7 @@
             </div>
             <div class="info">
             
-               My name is James. I live in Keller, Texas with my wife and four children. 
-               Keller is in the Dallas/Fort Worth metroplex, which is nice because you 
-               get pretty much anything you need.  The down side is that it can feel a 
-               bit crowded sometimes.  I grew up in rural Arizona, so I miss the open 
-               spaces now and then.  I enjoy riding my motorcycle, cooking, and online 
-               gaming, none of which I have much time for this days. 
+               Welcome to Girl Scout troup XXXX 
                   
             </div>
             </div>
