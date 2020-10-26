9d27a2400194606c67f2c7257add8b13e5c9cd8b
James C <jchell@byui.edu>
2020-10-25 21:51:02 -0500

    footer update

--- web/CSE341WebSite/common/footer.php
+++ web/CSE341WebSite/common/footer.php
@@ -1,12 +1,12 @@
 <div class="links">
             <ul>
-            Iterests:&nbsp &nbsp<li><a href="https://www.girlscouts.org/">Girl Scouts of America</a></li>
+            Related Sites:&nbsp &nbsp<li><a href="https://www.girlscouts.org/">Girl Scouts of America</a></li>
                <li><a href="https://www.girlscoutshop.com/">Girl Scouts Shop</a></li>
                <li><a href="https://www.gs-top.org/">GS Texas Oklahoma Planes</a></li>
-               <li><a href="https://www.churchofjesuschrist.org">Church of Jesus Christ of Laterday Saints</a></li>
+               <li><a href="https://makingfriends.com/">Making Friends</a></li>
            </ul>
          </div>
 
-<p>&copy; James Chell, All rights reserved.</p>
+<p>&copy;  All rights reserved.</p>
             <?php echo "The universal time is " . date("h:i:sa");?>
-            <p>Last updated 8 May, 2020</p>
+            <p>Last updated 25 October, 2020</p>
